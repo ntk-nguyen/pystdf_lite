33e6f79ca2e802c75b118c4b03131639bf0e105d
Nguyen Nguyen <ntk.nguyen@outlook.com>
2021-10-08 11:09:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import glob
+import os
 from pystdf.Extractor import write_data_frame
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    file_names = [
-        r'C:\Users\Nguyen Nguyen\Documents\PythonProjects\pystdf\data\lot2.stdf.gz',
-        r'C:\Users\Nguyen Nguyen\Downloads\stress_stdfs\M1BB-A0-W0---_4C66250-01_W01_20210520_230258_Datalog.stdf.gz',
-    ]
+    sample_data_dir = os.path.join(os.getcwd(), 'data')
+    file_names = glob.glob(os.path.join(sample_data_dir, '*.stdf.gz'))
     for f in file_names:
         write_data_frame(f)
