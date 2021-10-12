9d7b7ade14f6d95e94dd7ed76d22a36fb97ffdd3
Nguyen Nguyen <ntk.nguyen@outlook.com>
2021-10-12 13:40:31 -0500

    Modify setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='PySTDF-Lite',
-    version='0.2.1',
+    version='0.2.2',
     description='PYSTDF Lite is a Python library to extract STDF files into parametric and associated metadata into '
                 'wide data frames.',
     author='Nguyen T. Nguyen',
