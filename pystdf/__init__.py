50be9e5126e6e52faa7c8948c1c2d743f6d535f8
Nguyen Nguyen <ntk.nguyen@outlook.com>
2021-10-12 13:13:59 -0500

    Add setup.py and modify .gitignore

--- pystdf/__init__.py
+++ pystdf/__init__.py
@@ -17,4 +17,4 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 from pystdf.TableTemplate import TableTemplate
-# from pystdf.Indexing import *
+from pystdf import Extractor
