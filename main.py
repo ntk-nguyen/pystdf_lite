fd64cd9cb465b8b6199d6c0fd6a04420848e74ac
Nguyen Nguyen <ntk.nguyen@outlook.com>
2021-11-23 10:32:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# This is the main function to extract STDF files.
+# Running main.py will parse sample public STDF files in /data folder
 
 import glob
 import os
